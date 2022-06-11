8fc582f5efff5f5920989298ccfed9d0bbb208c5
Andriy Kulchytskyy <ak250578@gmail.com>
2022-06-12 00:04:50 +0200

    task 2 completed
    task 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,15 @@
 import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const LOCALSTORAGE_KEY = 'videoplayer - current - time';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
