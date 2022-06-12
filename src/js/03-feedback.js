81c545e673791e47a3a7f4332a17f38e3bf907f0
Andriy Kulchytskyy <ak250578@gmail.com>
2022-06-12 03:51:18 +0200

    task 3 completed
    task 3 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('[name="email"]'),
+  message: document.querySelector('[name="message"]'),
+  submit: document.querySelector('[type="submit"]'),
+};
+
+let storedObject = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (storedObject) {
+  refs.email.value = storedObject.email;
+  refs.message.value = storedObject.message;
+}
+
+refs.form.addEventListener('input', onInput);
+
+function onInput(event) {
+  const formElements = event.currentTarget.elements;
+  const objectToStore = {
+    email: formElements.email.value,
+    message: formElements.message.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(objectToStore));
+}
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+  localStorage.clear();
+
+  refs.email.value = '';
+  refs.message.value = '';
+}
