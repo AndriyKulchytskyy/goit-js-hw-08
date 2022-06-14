1fc06b8fb5ceedeffe60a19ed781f88581f94508
Andriy Kulchytskyy <ak250578@gmail.com>
2022-06-14 14:23:05 +0200

    promisess issue, try N:1
    promisess issue, try N:1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,12 @@ import throttle from 'lodash.throttle';
 const LOCALSTORAGE_KEY = 'videoplayer - current - time';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
 
 if (!JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))) {
   player.setCurrentTime(0);
 }
+
+const player = new Vimeo(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
