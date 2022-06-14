c8ed36985212e6ae75983b6b2167197b39762abf
Andriy Kulchytskyy <ak250578@gmail.com>
2022-06-14 15:09:03 +0200

    final 1
    final 1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,10 +10,31 @@ const player = new Vimeo(iframe);
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-  if (!localStorage.getItem(LOCALSTORAGE_KEY)) {
-    player.setCurrentTime(0);
-  }
+  // if (!localStorage.getItem(LOCALSTORAGE_KEY)) {
+  //   player.setCurrentTime(0);
+  // }
   localStorage.setItem(LOCALSTORAGE_KEY, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+// player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+player
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+    seconds = localStorage.getItem(LOCALSTORAGE_KEY);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        console.log(
+          'the time was less than 0 or greater than the video’s duration'
+        );
+        break;
+
+      default:
+        // some other error occurred
+        console.log('some other error occurred');
+        break;
+    }
+  });
